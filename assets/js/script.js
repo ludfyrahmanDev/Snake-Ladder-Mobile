40152e787a15bc0c44a335a83f2d09434298e927
ludfy rahman <ludfyr@gmail.com>
2024-08-22 13:53:33 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+window.onload = function() {
+    // check if file is index.html
+    if (window.location.pathname == '/index.html' || window.location.pathname == '/' || window.location.pathname == '') {
+      const splashScreen = document.querySelector('.splash-screen');
+  
+      // Simulate loading time
+      setTimeout(() => {
+        // splashScreen.style.display = 'none';
+        // redirect to the game
+        window.location.href = 'main-menu.html';
+        // Load main content or redirect to the game
+      }, 3000); // Replace 3000 with desired loading time in milliseconds
+    }
+  };
+  
+  // main menu script
+  // onclick button open attr link
+  function openLink(link) {
+    window.location.href = link;
+  }
+  // open back
+  function openBack() {
+    window.history.back();
+  }
+  // make function multiplayer and alert input count player
+  function multiplayer() {
+    const countPlayer = prompt('Enter the number of players');
+    if (countPlayer) {
+      // set max player
+      if (countPlayer > 4) {
+        alert('Maximum 4 players');
+      } else {
+        // redirect to the game
+        alert(`You have entered ${countPlayer} players`);
+        openLink('game.html');
+      }
+    }
+  }
+
+  // add loading every open page
+  window.addEventListener('load', () => {
+    // append element
+    const loading = document.createElement('div');
+    loading.classList.add('loading');
+    loading.innerHTML = '<img src="assets/img/logo.png" alt="loading" style="width:150px">Loading...';
+    document.body.appendChild(loading);
+    // remove element after 3s
+    setTimeout(() => {
+      loading.remove();
+    }, 500);
+
+  }); 
---
+++ index.html
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Game Splash Screen</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/animation.css">
+</head>
+
+<body>
+    <div class="splash-screen">
+        <img src="assets/img/logo.png" alt="Game Logo" class="logo">
+        <p class="loading-text">Loading...</p>
+    </div>
+    <script src="assets/js/script.js"></script>
+</body>
+
+</html>
